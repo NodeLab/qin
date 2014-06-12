467cd3a83af98eb40a7ab810ee188f322006b2be
elrrrrrrr <elrrrrrrr@gmail.com>
2014-06-12 16:48:39 +0800

    update gulp resize

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,12 @@ gulp.task('resize',function() {
 		.pipe(gulp.dest('case/sushi/1406-1/images/grid/'))
 	return	
 })
+
+gulp.task('logo',function(){
+	gulp.src("/Users/elr-mbp/Desktop/北京-天天中奖餐饮/*/logo.{jpg,jpeg,JPG}")
+		.pipe(gm(function(gmfile){
+			return gmfile.resize(180,180,"!").setFormat('jpg');
+		}))
+		.pipe(gulp.dest('/Users/elr-mbp/Desktop/temp/'))
+	return 
+})
