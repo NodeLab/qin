93dd7ce08c5e1e667293d39cf89dde23f8654304
elrrrrrrr <elrrrrrrr@gmail.com>
2014-06-15 20:42:48 +0800

    add jest to unit test

---
+++ __tests__/simple.js
@@ -0,0 +1,7 @@
+jest.dontMock('../lib/index');
+describe('test', function() {
+ it('adds 1 + 2 to equal 3', function() {
+   var sum = require('../lib/index');
+   expect(sum(1, 2)).toBe(3);
+ });
+});
