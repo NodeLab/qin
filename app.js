5002bc3831694ce9d4620ba4356a51b2094e21bc
elrrrrrrr <elrrrrrrr@gmail.com>
2014-06-20 14:35:19 +0800

    clean repo

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 var os = require('os');
-var connect = require('connect');
 var cwd = process.cwd();
 var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var index = require('./routes/index');
+var app = express();
 
 var openURL = function (url) {
   switch (process.platform) {
@@ -15,19 +22,9 @@ var openURL = function (url) {
       break;
     default:
       spawn('xdg-open', [url]);
-    // I use `spawn` since `exec` fails on my machine (Linux i386).
-    // I heard that `exec` has memory limitation of buffer size of 512k.
-    // http://stackoverflow.com/a/16099450/222893
-    // But I am not sure if this memory limit causes the failure of `exec`.
-    // `xdg-open` is specified in freedesktop standard, so it should work on
-    // Linux, *BSD, solaris, etc.
   }
 };
 
-/**
- * Get ip(v4) address
- * @return {String} the ipv4 address or 'localhost'
- */
 var getIPAddress = function () {
   var ifaces = os.networkInterfaces();
   var ip = '';
@@ -42,16 +39,60 @@ var getIPAddress = function () {
   return ip || "127.0.0.1";
 };
 
-var app = connect();
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded()); 
+//app.use(multer({dest:'./uploads/'}));
+app.use(cookieParser());
+//app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'case')));
+console.log(cwd)
+app.use(express.static(path.join(cwd)));
+// app.use(express.static(path.join('/Users/elr-mbp/code/dianping/dp_event/case/')));
+// app.use(express.static(path.join('/Users/elr-mbp/code/dianping/dp_event/case/')))
+
+app.use('/', index);
+
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
-app.use(connect.static(cwd));
-app.use(connect.directory(cwd));
-var port = process.argv[2] || 8080;
-app.listen(port, function () {
-  var url = "http://" + getIPAddress() + ":" + port;
-  console.log("Running at " + url);
-  openURL(url);
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
 });
+
+
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+  openURL("http://" + getIPAddress() + ":" + 3000 + "/index.html");
+});
+
+exports = module.exports = app;
+
