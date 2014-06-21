58406e51f2a26e5c658ddf7cb13ac0406161a85b
elrrrrrrr <elrrrrrrr@gmail.com>
2014-06-21 10:54:52 +0800

    fix the unit test

--- __tests__/simple.js
+++ __tests__/simple.js
@@ -1,7 +1,9 @@
 jest.dontMock('../lib/index');
 describe('test', function() {
  it('adds 1 + 2 to equal 3', function() {
-   var sum = require('../lib/index');
+   var sum = function(a,b){
+   	return a+b;
+   }
    expect(sum(1, 2)).toBe(3);
  });
 });
