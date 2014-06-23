20bb66c51d3393b5adcf36f6ac0e637cfdf4dc31
elrrrrrrr <elrrrrrrr@gmail.com>
2014-06-23 09:17:26 +0800

    clean floder

--- routes/users.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var user = require('../controllers/user')
-/* GET users listing. */
-//router.get('/',user.intro);
-router.post('/',user.intro);
-module.exports = router;
