6f99ec4de6e9cf1f7384bd7752247d54e19ce923
elrrrrrrr <elrrrrrrr@gmail.com>
2014-06-26 10:15:09 +0800

    change the interface

--- app.js
+++ app.js
@@ -79,12 +79,5 @@ app.use(function(err, req, res, next) {
     });
 });
 
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
-  openURL("http://" + getIPAddress() + ":" + 3000 + "/index.html");
-});
-
 exports = module.exports = app;
 
