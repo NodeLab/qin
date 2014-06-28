dfc69f2f44e3fa7b5bad7e48bca77a3b28adb334
elrrrrrrr <elrrrrrrr@gmail.com>
2014-06-28 14:34:46 +0800

    reset the assets

--- public/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-define(function(require,exports,module){	
-	var util = require('./util');
-	var hello = document.getElementById('hello');
-	hello.style.color = util.randomColor();
-	window.setInterval(function(){
-		hello.style.color = util.randomColor();
-	},1500);	
-})
