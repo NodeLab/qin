c1fc24cf42c4e3c1d8425078d7c6b3d2dba43565
elrrrrrrr <elrrrrrrr@gmail.com>
2014-06-29 23:32:52 +0800

    add intro.html

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,10 @@ var router = express.Router();
 var controller = require('../controllers/index')
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/intro', function(req, res) {
   //res.render('index', { title: 'Express' });
-  res.sendfile('views/index.html');
+  res.sendfile('intro.html');
 });
-
-router.get('/simple_form',function(req,res) {
-	res.render('simple_form',{ title : 'Simple Form'});
-});
-
-router.post('/uploader',function(req,res) {
-	console.log(req.files);
-})
 
 router.get('/prize/ajax/prizeDraw',controller.prize)
 
@@ -23,5 +15,7 @@ router.get('/ajax/json/account/info',controller.account)
 router.get('/prize/ajax/recordList',controller.recordList)
 
 router.get('/vote/ajax/ajaxVote',controller.vote)
+
+
 
 module.exports = router;
