64a78fe34421f5c7ca57c1354c875853c3597a2d
elrrrrrrr <elrrrrrrr@gmail.com>
2014-06-30 19:31:21 +0800

    replace the router

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/index')
+var config = require('../config.json')
 
 /* GET home page. */
+
+router.get('/test',function(req,res){
+	res.send('xixix')
+})
 router.get('/intro', function(req, res) {
-  //res.render('index', { title: 'Express' });
-  res.sendfile('intro.html');
+	res.sendfile('public/intro.html');
 });
+router.get('*',function(req,res){
+	res.header("Content-Type", "application/json; charset=utf-8");
+	var result = config.ajaxList[req.path] || "Hello World"
+	res.send(result)
+})
 
-router.get('/prize/ajax/prizeDraw',controller.prize)
 
-router.get('/ajax/json/account/info',controller.account)
 
-router.get('/prize/ajax/recordList',controller.recordList)
+// router.get('/prize/ajax/prizeDraw',controller.prize)
 
-router.get('/vote/ajax/ajaxVote',controller.vote)
+// router.get('/ajax/json/account/info',controller.account)
+
+// router.get('/prize/ajax/recordList',controller.recordList)
+
+// router.get('/vote/ajax/ajaxVote',controller.vote)
 
 
 
