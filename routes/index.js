ba6628bfd1fcf9a480c824c380ec3073ce31de47
elrrrrrrr <elrrrrrrr@gmail.com>
2014-06-30 20:02:42 +0800

    update md

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var config = require('../config.json')
 
 /* GET home page. */
 
+
 router.get('/test',function(req,res){
 	res.send('xixix')
 })
@@ -12,20 +13,12 @@ router.get('/intro', function(req, res) {
 	res.sendfile('public/intro.html');
 });
 router.get('*',function(req,res){
+	console.log(req)
 	res.header("Content-Type", "application/json; charset=utf-8");
 	var result = config.ajaxList[req.path] || "Hello World"
 	res.send(result)
 })
 
-
-
-// router.get('/prize/ajax/prizeDraw',controller.prize)
-
-// router.get('/ajax/json/account/info',controller.account)
-
-// router.get('/prize/ajax/recordList',controller.recordList)
-
-// router.get('/vote/ajax/ajaxVote',controller.vote)
 
 
 
