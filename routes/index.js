b84389eb31a999903e01c434f8b36d9fd67730e6
elrrrrrrr <elrrrrrrr@gmail.com>
2014-07-05 17:44:12 +0800

    enable default config

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var cwd = process.cwd();
 var controller = require('../controllers/index')
-var config = require(cwd+'/config.json')
+var config = require('../utils/getConfig')
 
 /* GET home page. */
 
@@ -13,7 +13,6 @@ router.get('/intro', function(req, res) {
 	res.sendfile('public/intro.html');
 });
 router.get('*',function(req,res){
-	console.log(req)
 	res.header("Content-Type", "application/json; charset=utf-8");
 	var result = config.ajaxList[req.path] || "Hello World"
 	res.send(result)
