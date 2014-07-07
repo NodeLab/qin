11fefd273a04d7f6413f9c6ac6ae4123733b2614
elrrrrrrr <elrrrrrrr@gmail.com>
2014-07-07 23:36:33 +0800

    rebase the test case

--- __tests__/simple.js
+++ __tests__/simple.js
@@ -1,9 +1,6 @@
 jest.dontMock('../lib/index');
 describe('test', function() {
- it('adds 1 + 2 to equal 3', function() {
-   var sum = function(a,b){
-   	return a+b;
-   }
-   expect(sum(1, 2)).toBe(3);
+ it('hello', function() {
+   expect(true).toBe(true);
  });
 });
