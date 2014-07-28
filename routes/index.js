30bc1bdf9959bc6d9fe15d4500026a5dc5da4679
elrrrrrrr <elrrrrrrr@gmail.com>
2014-07-28 21:22:13 +0800

    enable the mock & api

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,26 @@ var request = require('request')
 router.get('/test',function(req,res){
 	res.send('xixix')
 })
+
 router.get('/intro', function(req, res) {
 	res.sendfile('public/intro.html');
 });
+
 router.get('*',function(req,res){
-	// res.header("Content-Type", "application/json; charset=utf-8");
-	var url = 'http://www.baidu.com/' + req.url
-  	req.pipe(request(url))
-  		.pipe(res)
+
+	res.header("Content-Type", "application/json; charset=utf-8");
+	var _r = config.ajaxList[req.path]
+
+	if (_r) {
+		res.send(_r)
+		return 
+	} else {
+		var url = config.apiPath + req.url
+	  	req.pipe(request(url))
+	  		.pipe(res)
+	  	return 
+	}
 })
+
 
 module.exports = router;
