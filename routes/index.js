03d8127ecb164ef2d7cbdeae40c87f14ffd13017
elrrrrrrr <elrrrrrrr@gmail.com>
2014-07-29 23:03:54 +0800

    covert controller 2 js

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,34 @@
 var express = require('express');
+var request = require('request')
 var router = express.Router();
 var cwd = process.cwd();
+
 var controller = require('../controllers/index')
 var config = require('../utils/getConfig')
-var request = require('request')
 
 /* GET home page. */
 
-router.get('/test',function(req,res){
-	res.send('xixix')
+router.get('/test',function(req, res) {
+	
+	controller.test(req, res)
 })
 
 router.get('/intro', function(req, res) {
 	res.sendfile('public/intro.html');
 });
 
-router.get('*',function(req,res){
+//console 
 
+router.get('/console', function(req, res) {
+	res.sendfile('public/console.html')
+})
+
+router.post('/console', function(req, res){
+
+})
+
+//enable the proxy
+router.get('*',function(req,res){
 	res.header("Content-Type", "application/json; charset=utf-8");
 	var _r = config.ajaxList[req.path]
 
@@ -26,9 +38,9 @@ router.get('*',function(req,res){
 	} else {
 		var url = config.apiPath + req.url
 		console.log(url)
-	  	req.pipe(request(url))
-	  		.pipe(res)
-	  	return 
+	  req.pipe(request(url))
+	  	.pipe(res)
+	  return 
 	}
 })
 
