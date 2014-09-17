726acba5a5f994083a17ef822994e1303daf6708
Arvo <ArvoGuo@gmail.com>
2014-09-17 10:05:36 +0800

    fix the require cache bug in config

--- utils/getConfig.js
+++ utils/getConfig.js
@@ -1,14 +1,18 @@
 var fs = require("fs-extra")
 var cwd = process.cwd()
 var existsSync = fs.existsSync
-var config 
 
-Object.keys(require.cache)
+/*
+	
+*/
+exports.config = function(sync){
+	
+	Object.keys(require.cache)
     .forEach( function(key) {
         delete require.cache[key];     
     })
 
-config = existsSync(cwd + '/config.json') ? require( cwd+'/config.json' ) : require( __dirname + '/../config.json' )
+	return existsSync(cwd + '/config.json') ? require( cwd+'/config.json' ) : require( __dirname + '/../config.json' )
+}
 
-exports = module.exports = config 
 
