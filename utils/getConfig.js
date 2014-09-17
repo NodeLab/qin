b386f2ca9f575a09e9ba7d0cc663b4674a6d3854
elrrrrrrr <elrrrrrrr@gmail.com>
2014-09-17 22:03:39 +0800

    fix test case

--- utils/getConfig.js
+++ utils/getConfig.js
@@ -3,16 +3,15 @@ var cwd = process.cwd()
 var existsSync = fs.existsSync
 
 /*
-	
+    
 */
 exports.config = function(sync){
-	
-	Object.keys(require.cache)
-    .forEach( function(key) {
-        delete require.cache[key];     
-    })
+    
+    Object.keys(require.cache)
+        .forEach( function(key) {
+            delete require.cache[key];     
+        })
 
-	return existsSync(cwd + '/config.json') ? require( cwd+'/config.json' ) : require( __dirname + '/../config.json' )
+    return existsSync(cwd + '/config.json') ? require( cwd+'/config.json' ) : require( __dirname + '/../config.json' )
 }
-
 
