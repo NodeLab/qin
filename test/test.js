5559949b1318b6442849e3ccecfc29c3c30ec017
Arvo <ArvoGuo@gmail.com>
2014-09-22 23:12:17 +0800

    update mocha test.js

--- test/test.js
+++ test/test.js
@@ -1,33 +1,56 @@
 var assert = require("assert"),
-    fs = require('fs');
+  fs = require('fs'),
+  app = require('../app'),
+  request = require('request'),
+  config = require('../utils/getConfig').config();
+ 
 
 
-describe('Utils', function(){
+describe('Utils', function() {
 
   describe('auto reload config.json', function() {
-    
+
     var config = require('../utils/getConfig').config();
     var src = {};
 
     for (var i in config) {
       src[i] = config[i];
     }
-
     after(function() {
       fs.writeFile('config.json', JSON.stringify(src, null, "    "));
     });
 
-  	it('should update reload', function(done){
+    it('should update reload', function(done) {
       config.t = 'true'
-  		fs.writeFile('config.json', JSON.stringify(config), function(err) {
+      fs.writeFile('config.json', JSON.stringify(config), function(err) {
         if (err) console.log(err);
-        
+
         var changed = require('../utils/getConfig').config();
         assert(true, changed.t)
         done()
       });
 
 
-  	})
+    })
+  })
+
+})
+describe('Routes', function() {
+  describe('console req', function() {
+    var sever;
+    before(function() {
+      sever=app.listen(3000);
+    })
+    after(function() {
+      sever.close();
+    })
+    it('should ', function(done) {
+      request.get('http://localhost:3000/prize/ajax/prizeDraw?groupId=198', function(err,res, body) {
+        assert(config.ajaxList[res.req.path])
+        done();
+      });
+    })
+
+
   })
 })
