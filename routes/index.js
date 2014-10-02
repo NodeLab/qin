4db20c12eba67c8a8c28a370a01411c8a776b4c8
elrrrrrrr <elrrrrrrr@gmail.com>
2014-10-02 20:45:50 +0800

    fit post requests

--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,7 @@ router.get('*',function(req,res){
 		//custom
 		var type = _r.type;
 		var isEqual = true;
+		if (type.toLowerCase() != 'get') return
 		for (var i in _r.query) {
 			if (_r.query[i] != req.query[i] && _r.query[i] != '*') {
 				isEqual = false;
@@ -59,10 +60,34 @@ router.get('*',function(req,res){
 })
 
 router.post('*',function(req, res) {
-	var config = require('../utils/getConfig')
-	var url = config.apiPath + req.url
-	req.pipe(request.post(url))
-		.pipe(res)
+	res.header("Content-Type", "application/json; charset=utf-8");
+	var config = require('../utils/getConfig');
+	var _r = config.ajaxList[req.path];
+
+	if (typeof _r == 'object' && _r.type = 'post') {
+		var type = _r.type;
+		var isEqual = true;
+		if (type.toLowerCase() != 'post') return
+		for (var i in _r.query) {
+			if (_r.body[i] != req.body[i] && _r.body[i] != '*') {
+				isEqual = false;
+				break;
+			}
+		}
+		if (isEqual) {
+			res.send(_r.result);
+		} else {
+			res.send(_r.reject || '参数填写错误 请重新填写');
+		}
+	} else if ('apiPath' in config){
+		//pipe
+		var url = config.apiPath + req.url;
+		console.log('转发请求至'+url);
+		req.pipe(request.post(url))
+			.pipe(res)	
+	} else {
+		res.send({'msg': '接口未定义，请仔细检查'})
+	}
 	return 
 })
 module.exports = router;
