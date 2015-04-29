46a7d29edb4aa8bddc012cab76ae493b92243f96
elrrrrrrr <elrrrrrrr@gmail.com>
2015-04-29 11:25:59 +0800

    fix: fix server-index intercept post request
    - 抽离默认router index.js 中 post 和 get 路由
    - 定义ftl / server-index ／ static 的挂载顺序

---
+++ routes/post.js
@@ -0,0 +1,40 @@
+var request = require('request')
+var router = require('express').Router();
+var cwd = process.cwd();
+var path = require('path');
+
+// server-index will intercept post request
+router.post('*',function(req, res) {
+	res.header("Content-Type", "application/json; charset=utf-8");
+	var config = require('../utils/getConfig').config();
+	var _r = config.ajaxList[req.path];
+	if (typeof _r == 'object' && _r.type == 'post') {
+		var type = _r.type;
+		var isEqual = true;
+    if (type.toLowerCase() !== 'post') {
+      return;
+    } 
+		for (var i in _r.query) {
+			if (_r.body[i] != req.body[i] && _r.body[i] != '*') {
+				isEqual = false;
+				break;
+			}
+		}
+		if (isEqual) {
+			res.send(_r.result);
+		} else {
+			res.send(_r.reject || '参数填写错误 请重新填写');
+		}
+	} else if ('apiPath' in config){
+		//pipe
+		var url = config.apiPath + req.url;
+		console.log('转发请求至'+url);
+		req.pipe(request.post(url))
+			.pipe(res)	
+	} else {
+		res.send({'msg': '接口未定义，请仔细检查'})
+	}
+	return 
+})
+
+module.exports = router;
