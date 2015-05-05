d3a44b7420eedea9aa72bdaae7f321bef8fd6032
Arvo <ArvoGuo@gmail.com>
2015-05-05 11:20:58 +0800

    feat: adjust filestructure，add autoReload

---
+++ public/reload/reload.js
@@ -0,0 +1,32 @@
+(function() {
+
+  var socket = io.connect();
+
+  socket.on('reloadCss', function () {
+    reloadCss();
+  });
+
+  socket.on('reloadAll', function() {
+    reloadAll();
+  });
+
+  socket.on('test',function(data) {
+    alert(data.message);
+  });
+
+  function reloadCss() {
+    var links = document.getElementsByTagName("link");
+    for (var i = 0, len = links.length; i < len; i ++) {
+      var link = links[i];
+      if (link.href.indexOf("css") > -1) {
+        var string = link.href.replace(/^([^\?]*)([\?]*.*)/, '$1' + '?random=' + (new Date).getTime() + (Math.random() * 10000));
+        link.href  = string;
+      }
+    }
+  }
+
+  function reloadAll() {
+    location.reload();
+  }
+
+})();
