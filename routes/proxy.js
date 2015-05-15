3eeda0f737ebf727b89230ef5e68111006272ae4
Arvo <ArvoGuo@gmail.com>
2015-05-15 20:10:14 +0800

    feat: restrust routes - 重构路由 - fixed #4

---
+++ routes/proxy.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router  = express.Router();
+var request = require('request');
+
+router.all('*', function(req, res, next) {
+  res.header("Content-Type", "application/json; charset=utf-8");
+  var config = require('../utils/getConfig').config();
+  if (!config.apiPath) {
+    next();
+    return;
+  }
+  if (config.apiPath.status) {
+    if (!config.apiPath.url) {
+      res.send({'msg': '接口未定义，请仔细检查'});
+      return;
+    }
+    if (!req.url) {
+      console.log('请求路径错误, 请检查请求路径');
+      return;
+    }
+    var url = config.apiPath.url + req.url;
+    console.log('已开启代理，转发请求至' + url);
+    req.pipe(request.post(url))
+      .pipe(res);
+  } else {
+    next();
+  }
+  return;
+});
+
+module.exports = router;
