ffdd7499b09c4b9fc8f5f836fa5edba73fa05d19
elrrrrrrr <elrrrrrrr@gmail.com>
2015-05-16 19:56:07 +0800

    style: correct the code style
    - 重构代码

--- routes/mock.js
+++ routes/mock.js
@@ -1,43 +1,44 @@
+'use strict';
 var express = require('express');
-var router  = express.Router();
+var router = express.Router();
 
 router.all('*', function(req, res, next) {
-  res.header("Content-Type", "application/json; charset=utf-8");
+  res.header('Content-Type', 'application/json; charset=utf-8');
   var config = require('../utils/getConfig').config();
-  var _r = config.ajaxList[req.path];
+  var r = config.ajaxList[req.path];
 
-  if (!_r) {
+  if (!r) {
     next();
     return;
   }
   var property;
   var isEqual = true;
 
-  if (req.method == 'GET') {
-    if (!('result' in _r)) {
-      res.send(_r);
+  if (req.method === 'GET') {
+    if (!('result' in r)) {
+      res.send(r);
     }
     property = 'query';
   }
 
-  if (req.method == 'POST') {
-    if (_r.type.toLowerCase() !== 'post') {
+  if (req.method === 'POST') {
+    if (r.type.toLowerCase() !== 'post') {
       return;
     }
     property = 'body';
   }
 
-  for (var i in _r[property]) {
-    if (_r[property][i] != req[property][i] && _r[property][i] != '*') {
+  for (var i in r[property]) {
+    if (r[property][i] !== req[property][i] && r[property][i] !== '*') {
       isEqual = false;
       break;
     }
   }
 
   if (isEqual) {
-    res.send(_r.result);
+    res.send(r.result);
   } else {
-    res.send(_r.reject || '参数填写错误 请重新填写');
+    res.send(r.reject || '参数填写错误 请重新填写');
   }
   return;
 });
